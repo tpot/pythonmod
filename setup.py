b44ca3551642199792228e2bc1eecaf27371b6ec
Tim Potter <tpot@frungy.org>
2023-02-05 07:01:23 +1100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='my_package',
+	version='0.1',
+	description='A sample Python package',
+	author='Tim Potter',
+	author_email='tpot@frungy.org',
+	packages=['my_package'],
+	install_requires=[
+	],
+)
